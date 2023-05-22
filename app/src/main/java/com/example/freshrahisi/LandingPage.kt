d4a8d8c14428aad9ee21c961363cb9e0c47a1e7b
Kilas-aniso <kilasaniso@gmail.com>
2023-05-22 12:43:46 +0300

    updates

--- app/src/main/java/com/example/freshrahisi/LandingPage.kt
+++ app/src/main/java/com/example/freshrahisi/LandingPage.kt
@@ -15,27 +15,38 @@ class LandingPage: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing_page)
+
+
+
+
+
+
         btnManage=findViewById(R.id.btnManage)
-        btnAdd=findViewById(R.id.btnAdd)
-        btnAddProduct=findViewById(R.id.btnAddproduct)
-        btnCheck=findViewById(R.id.btnCheck)
-        btnView=findViewById(R.id.btnView)
         btnManage.setOnClickListener {
             val intent = Intent(this,ManageOrders::class.java)
             startActivity(intent)
         }
+
+
+        btnAdd=findViewById(R.id.btnAdd)
         btnAdd.setOnClickListener {
             val intent=Intent(this,AddCategory::class.java)
             startActivity(intent)
         }
+
+        btnAddProduct=findViewById(R.id.btnAddproduct)
         btnAddProduct.setOnClickListener {
             val intent=Intent(this,AddProduct::class.java)
             startActivity(intent)
         }
+
+        btnCheck=findViewById(R.id.btnCheck)
         btnCheck.setOnClickListener {
             val intent=Intent(this,Feedback::class.java)
             startActivity(intent)
         }
+
+        btnView=findViewById(R.id.btnView)
         btnView.setOnClickListener {
             val intent=Intent(this,Products::class.java)
             startActivity(intent)
--- app/src/main/java/com/example/freshrahisi/MamaMboga.kt
+++ app/src/main/java/com/example/freshrahisi/MamaMboga.kt
@@ -4,17 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MamaMboga : AppCompatActivity() {
     lateinit var btnLogin: Button
     lateinit var btnRegistration: Button
+    lateinit var tilEmail:TextInputLayout
+    lateinit var tilPassword:TextInputLayout
+    lateinit var etEmail:TextInputEditText
+    lateinit var etPassword:TextInputEditText
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mama_mboga)
         btnLogin=findViewById(R.id.btnLogin)
         btnLogin.setOnClickListener {
-            var intent= Intent( this, LandingPage::class.java)
-            startActivity(intent)
+          validation()
+            clearErrors()
         }
         btnRegistration=findViewById(R.id.btnRegistration)
         btnRegistration.setOnClickListener {
@@ -22,4 +30,27 @@ class MamaMboga : AppCompatActivity() {
             startActivity(intent)
         }
     }
+     fun validation(){
+        val email=etEmail.text.toString()
+        val password=etPassword.text.toString()
+        var error=false
+
+        if(email.isBlank()){
+            tilEmail.error = "Please enter your Email"
+            error = true
+        }
+        if(password.isBlank()){
+            tilPassword.error = "Input your password"
+            error = true
+        }
+        if(!error){
+            var intent= Intent( this, LandingPage::class.java)
+            startActivity(intent)
+        }
+    }
+    fun clearErrors(){
+        tilEmail.error = null
+        tilPassword.error = null
+    }
+
     }
